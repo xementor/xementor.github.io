051332452b64309df9c78e7605091f7f30c307cd
xementor <ahammedzonaid@gmail.com>
2023-09-02 10:45:13 +0600

    add navbar

---
+++ src/components/Menu.svelte
@@ -0,0 +1,49 @@
+<ul class="menu bg-base-200 lg:menu-horizontal rounded-box">
+	<li>
+		<a>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-5 h-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z"
+				/>
+			</svg>
+
+			Projects
+		</a>
+	</li>
+	<li>
+		<a>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-5 h-5"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m6 4.125l2.25 2.25m0 0l2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+				/>
+			</svg>
+
+			Blog
+			<span class="badge badge-sm badge-warning">NEW</span>
+		</a>
+	</li>
+	<li>
+		<a>
+			Resume
+			<span class="badge badge-xs badge-info" />
+		</a>
+	</li>
+</ul>
