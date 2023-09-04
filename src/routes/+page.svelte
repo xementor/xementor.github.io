aae0583cc108190a624b82bc4f259d2bd25b045d
xementor <ahammedzonaid@gmail.com>
2023-09-04 18:39:43 +0600

    add sidebar for small screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,43 +12,58 @@
 	import Work from "../components/Work.svelte"
 </script>
 
-<div>
-	<section id="section1">
-		<Hero />
-	</section>
-	<section id="section2">
-		<AboutMe />
-	</section>
-	<section id="section3">
-		<Work />
-	</section>
-	<section id="section4">
-		<Project />
-	</section>
-	<section id="section5">
-		<Blog />
-	</section>
-	<section id="section6">
-		<Contact />
-	</section>
-</div>
+<div class="drawer drawer-end">
+	<input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+	<!-- Page content here -->
+	<div>
+		<section id="section1">
+			<Hero />
+		</section>
+		<section id="section2">
+			<AboutMe />
+		</section>
+		<section id="section3">
+			<Work />
+		</section>
+		<section id="section4">
+			<Project />
+		</section>
+		<section id="section5">
+			<Blog />
+		</section>
+		<section id="section6">
+			<Contact />
+		</section>
+	</div>
 
-<StepMenu />
+	<StepMenu />
 
-<div class="fixed bottom-0 right-0 inline-block">
-	<button class="btn btn-square btn-circle">
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			class="h-5 w-5"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke="currentColor"
-			><path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				d="M4 6h16M4 12h8m-8 6h16"
-			/></svg
+	<div class="drawer-content">
+		<label
+			for="my-drawer-4"
+			class="btn btn-circle fixed bottom-0 right-0 md:hidden"
 		>
-	</button>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-5 w-5"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M4 6h16M4 12h8m-8 6h16"
+				/></svg
+			>
+		</label>
+	</div>
+	<div class="drawer-side z-20">
+		<label for="my-drawer-4" class="drawer-overlay" />
+		<ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+			<!-- Sidebar content here -->
+			<li><a href="#section1">Sidebar Item 1</a></li>
+			<li><a href="#section2">Sidebar Item 1</a></li>
+		</ul>
+	</div>
 </div>
