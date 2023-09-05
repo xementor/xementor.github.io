e4819a0eb3c64cb971ec31ae7d4fe05858f8cddc
xementor <ahammedzonaid@gmail.com>
2023-09-05 10:25:25 +0600

    fix themes

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
 	import { autoClickedScrolled, canHideNavbar } from "../store/navhide"
 	import Menu from "./Menu.svelte"
+	import { Icon, Moon, Sun, Bars3CenterLeft } from "svelte-hero-icons"
 
-	let isDarkTheme = true
 	// navbarStore.js
 
 	let prevScrollpos = 0
 	let runnigY = 0
 	let theme = ""
 	$: active_class = $canHideNavbar ? "top-[-100px]" : "top-0"
+	$: icon = darkThemeName.includes(theme) ? Sun : Moon
 
-	enum lightThemeName {
-		"light",
-		"retro",
-	}
-	enum darkTheme {
-		"dracula",
-		"garden",
-		"dark",
+	const lightThemeName = ["retro", "light"]
+	const darkThemeName = ["dracula", "garden", "dark"]
+	const allThemes = {
+		all: [...lightThemeName, ...darkThemeName],
+		defaultLight: lightThemeName[0],
+		defaultDark: darkThemeName[0],
 	}
 
 	function toggle() {
-		isDarkTheme = !isDarkTheme // Toggle the theme state
-		theme = isDarkTheme ? "graden" : "retro"
+		theme = !darkThemeName.includes(theme)
+			? darkThemeName[0]
+			: lightThemeName[0]
 		window.document.body.setAttribute("data-theme", theme)
 	}
 	function setTheme(name: string) {
@@ -52,19 +52,7 @@
 			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 			<!-- svelte-ignore a11y-label-has-associated-control -->
 			<label tabindex="0" class="btn btn-ghost lg:hidden">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h8m-8 6h16"
-					/></svg
-				>
+				<Icon src={Bars3CenterLeft} size="24" />
 			</label>
 			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 			<div
@@ -93,30 +81,18 @@
 		<div class="dropdown dropdown-end dropdown-hover">
 			<button class="btn btn-ghost btn-circle" on:click={toggle}>
 				<div class="indicator">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-6 h-6"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-						/>
-					</svg>
+					<Icon src={icon} size="24" />
 				</div>
 			</button>
 			<ul
 				class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
 			>
-				<li class="btn-active">
-					<button on:click={() => setTheme("light")}>Light</button>
-				</li>
-				<li><button on:click={() => setTheme("dark")}>Dark</button></li>
-				<li><button on:click={() => setTheme("retro")}>Retro</button></li>
+				{#each allThemes.all as t}
+					<li class:btn-active={t == theme}>
+						<button on:click={() => setTheme(t)}>{t}</button>
+					</li>
+				{/each}
+				<!-- <li><button on:click={() => setTheme("retro")}>Retro</button></li> -->
 			</ul>
 		</div>
 	</div>
