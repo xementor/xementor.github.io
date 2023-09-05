31e749362f37b036670e2b7969b078b1b2c42094
xementor <ahammedzonaid@gmail.com>
2023-09-05 23:09:33 +0600

    add blogs data

--- src/db/generator.cjs
+++ src/db/generator.cjs
@@ -23,11 +23,33 @@ const projects = [
     desc: "It is a interactive platform to teach kids alphabet.It is a interactive platform to teach kids alphabet.",
   },
 ]
+const blogs = [
+  {
+    badge: "Cool",
+    desc: "it is a short des",
+    filename: "filename.md",
+    tags: ["ami", "tmi"],
+    thumbnail: "project.jpg",
+    title: "vong",
+    date: "a",
+  },
+  {
+    badge: "Cool",
+    desc: "it is a short des",
+    filename: "filename.md",
+    tags: ["ami", "tmi"],
+    thumbnail: "project.jpg",
+    title: "vong",
+    date: "",
+  },
+]
 const jsonData = JSON.stringify({ projects }, null, 2)
+const jsonData2 = JSON.stringify({ blogs }, null, 2)
 
 
 
 // Save the JSON data to a file
 fs.writeFileSync("./src/db/projects.json", jsonData)
+fs.writeFileSync("./src/db/blogs.json", jsonData2)
 
 console.log("JSON data saved to project.json",)
