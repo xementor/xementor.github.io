e183d2df4f6e58d1e0e8e980a48f5b4f27720b3d
xementor <ahammedzonaid@gmail.com>
2023-09-06 08:50:40 +0600

    resume added

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -3,5 +3,7 @@
 </script>
 
 <div>
-	<PdfViewer url="resume.pdf" pageNum={2} />
+	<PdfViewer
+		url={"https://raw.githubusercontent.com/xementor/xementor.github.io/72e4194090caf9303c6dce93eab0f48effb5f6b8/db/resume.pdf"}
+	/>
 </div>
